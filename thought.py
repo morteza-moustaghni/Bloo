028a0c2ea9f196deecadc42baec447e8c7a7e0a7
Morteza <morteza.moustaghni@gmail.com>
2019-07-18 22:15:55 +0200

    Update thought.py

--- thought.py
+++ thought.py
@@ -2,16 +2,26 @@ from operator import attrgetter
 import time
 
 class Thought:
-	def __init__(self, name, thoughtId, executed):
+	def __init__(self, name, thoughtId, executed, poppable):
 		self.name = str(name)
 		self.thoughtId = float(thoughtId)
 		self.executed = bool(executed)
+		self.poppable = bool(poppable)
 	def __repr__(self):
 		return repr((self.name))
+	def pop(self):
+		if(self.poppable = True):
+			return Thought(self.name, self.thoughtId, True, self.poppable)
+		else:
+			print("not able to remove thought")
 
 
-item1 = Thought("main", time.ctime(time.time()), False)
+thoughtqueue = [Thought("main", time.time(), False, False)]
 
-while True:
-	print(item1.thoughtId)
+while(thoughtqueue.len() != 0):
+	thoughtqueue[thoughtqueue.len()].pop()
+	rnd = int(random.random() * 100)
+	if(rnd <= 25):
+		thoughtqueue.append(Thought("Thought " + str(rnd), time.time(), False, True))
 	time.sleep(1)
+
