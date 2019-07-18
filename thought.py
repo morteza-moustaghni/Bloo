22c51bdda4dee016ddf6ecc64568760e9c0141ca
Morteza <morteza.moustaghni@gmail.com>
2019-07-18 21:41:23 +0200

    Update thought.py

--- thought.py
+++ thought.py
@@ -1,13 +1,17 @@
 from operator import attrgetter
+import time
 
 class Thought:
-	def __init__(self, name, id, executed):
+	def __init__(self, name, thoughtId, executed):
 		self.name = str(name)
-		self.id = str(id)
+		self.thoughtId = int(thoughtId)
 		self.executed = bool(executed)
 	def __repr__(self):
-		return repr((self.name, self.id, self.executed))
+		return repr((self.name))
 
 
-item1 = Thought("main", "0", False)
-print(item1)
+item1 = Thought("main", 0, False)
+
+while True:
+	print(item1)
+	time.sleep(1)
