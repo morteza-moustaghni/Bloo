1359b2d2531ac4bfa043081b691c02c21c814bb1
Morteza <morteza.moustaghni@gmail.com>
2019-12-29 21:04:08 +0100

    removed unnecesary files

--- pdfreader.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-pdfFileObj = open('No project is an island - Linking projects to history and context (Engwall 2003).pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-print(pdfReader.numPages)
-pageObj = pdfReader.getPage(0)
-text = pageObj.extractText()
-print(text)
