853d7963d74ccdc2bf7cb5f19fad77238dc63a3a
Morteza <morteza.moustaghni@gmail.com>
2020-02-18 22:58:51 +0100

    Update admin-base.py

--- admin-base.py
+++ admin-base.py
@@ -1,9 +1,23 @@
+from flask_admin.contrib.sqla import ModelView
 from flask import Flask
 from flask_admin import Admin
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
 
 admin = Admin(app, name='digicom', template_mode='bootstrap3')
-	
+
+@app.route("/")
+def home():
+	return "HOME"	
+
+class HelloWorld(Resource):
+	def get(self):
+		return{'hello', 'world'}
+
+api.add_resource(HelloWorld, '/')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
