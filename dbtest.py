9b68035dc4ef1bbff395c8f67acfa9973ce2c609
Morteza Moustaghni <morteza.moustaghni@gmail.com>
2020-02-21 06:34:26 +0100

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://PythonMysql:test123@localhost/python_test_1'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://PythonMySQL:test123@localhost/python_test_1'
 
 # Test if it works
 engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
