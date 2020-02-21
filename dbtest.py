3793dce0a007404a0df36b3af113bcca8f1539b3
Morteza Moustaghni <morteza.moustaghni@gmail.com>
2020-02-21 06:51:54 +0100

    Delete dbtest.py

--- dbtest.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://PythonMySQL:test123@localhost/python_test_1'
-
-# Test if it works
-engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
-print(engine.table_names())
