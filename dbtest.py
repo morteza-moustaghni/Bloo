87b9366c18d1409c305a86ac58e1019dcc293494
Morteza Moustaghni <morteza.moustaghni@gmail.com>
2020-02-26 04:28:20 +0100

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,3 +1,30 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+req = Request('https://www.willys.se/butiker/alla', headers={'User-Agent': 'Mozilla/5.0'})
+
+page_html = urlopen(req).read()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
